cea21db904588dd16d727f657d77358408d79640
Joyce <joycequintino11@gmail.com>
2018-04-07 14:51:04 -0300

    Atualização das cardinalidades e chaves estrangeiras

--- RedeCegonha/RedeCegonha/Modelagem/ModelagemLais.Designer.cs
+++ RedeCegonha/RedeCegonha/Modelagem/ModelagemLais.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\bolsista\Documents\Joyce\RedeCegonha\RedeCegonha\Modelagem\ModelagemLais.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\bolsista\Documents\Joyce\Projetolp2\RedeCegonha\RedeCegonha\Modelagem\ModelagemLais.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
